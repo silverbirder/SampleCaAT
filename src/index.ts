f4bff99ac60c941aaf341f61f7bc59e6174f7e1b
shibamoto <silverbirder@gmail.com>
2020-01-23 23:25:23 +0900

    Develop the main process and utils

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import CaAT from '@silverbirder/caat'
+import * as CaAT from '@silverbirder/caat'
+import {copyDate, getLatestMonday, skipDay} from "./utils";
 
-function getMembers(){
-    const id: string = 'testUser@gmail.com';
-    const config: CaAT.IMemberConfig = {
-        startDate: new Date(),
-        endDate: new Date(),
-        everyMinutes: 15,
-        cutTimeRange: [],
-        ignore: new RegExp(''),
+function getMembers() {
+    const id: string = PropertiesService.getScriptProperties().getProperty('ID');
+
+    // setup date
+    const cutTimeRange: Array<CaAT.IRange> = [];
+    const monday: Date = new Date(getLatestMonday().setHours(0, 0, 0));
+    const friday: Date = new Date(skipDay(monday, 4).setHours(23, 59, 59));
+    for (const d = copyDate(monday); d <= friday; d.setDate(d.getDate() + 1)) {
+        cutTimeRange.push({
+            from: new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 0, 0),
+            to: new Date(d.getFullYear(), d.getMonth(), d.getDate(), 13, 0, 0),
+        })
     }
+
+    const config: CaAT.IMemberConfig = {
+        startDate: monday,
+        endDate: friday,
+        everyMinutes: 15,
+        cutTimeRange: cutTimeRange,
+        ignore: new RegExp(''),
+    };
     const member: CaAT.IMember = new CaAT.Member(id, config);
     const schedules: Array<CaAT.ISchedule> = member.fetchSchedules();
-    Logger.log(schedules);
 }
--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,12 @@ export function getLatestMonday() :Date{
     return new Date(now.setDate(now.getDate() - (now.getDay() + 6) % 7 + 7));
 }
 
-export function getLatestFriday(): Date{
-    const now: Date = new Date();
-    return new Date(now.setDate(now.getDate() - (now.getDay() + 2) % 7 + 7));
+export function skipDay(d: Date, skip: number): Date{
+    const copy: Date = copyDate(d);
+    copy.setDate(copy.getDate()+skip);
+    return copy;
+}
+
+export function copyDate(d: Date) {
+    return new Date(d.getTime());
 }
