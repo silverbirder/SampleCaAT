7a94daac2bfaa5a505f5d741da58118754bd0a31
shibamoto <silverbirder@gmail.com>
2020-01-24 07:25:32 +0900

    Develop the main process and utils

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {copyDate, getLatestMonday, skipDay} from "./utils";
 import Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
 import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
 
-function getMembers() {
+function main() {
     // prepare property
     const userId: string = PropertiesService.getScriptProperties().getProperty('USER_ID');
     const spreadSheetId: string = PropertiesService.getScriptProperties().getProperty('SPREAD_SHEET_ID');
@@ -61,4 +61,23 @@ function getMembers() {
         [userId, `${totalAssignMinutes/60}`],
     ];
     targetSheet.getRange(`A1:B2`).setValues(cell);
+
+    // TODO: Make the group process ...
+    const members: Array<CaAT.IGroupMember> = [{
+        name: userId,
+        match: new RegExp(null),
+    }];
+    const holidayWords: CaAT.IHolidayWords = {
+        morning: new RegExp(null),
+        afternoon: new RegExp(null),
+        all: new RegExp(null),
+    };
+    const groupConfig: CaAT.IGroupConfig = {
+        startDate: monday,
+        endDate: friday,
+        members: members,
+        holidayWords: holidayWords,
+    };
+    const group: CaAT.IGroup = new CaAT.Group(userId, groupConfig);
+    const holidays: Array<CaAT.IHoliday> = group.fetchHolidays();
 }
