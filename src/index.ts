655c6953a044d192c34b34c37d0db8fb812c357c
shibamoto <silverbirder@gmail.com>
2020-01-23 19:34:14 +0900

    Add sample code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-function sample() {
-    // @ts-ignore
-    const member = new CaAT.MemberImpl('testUser@gmail.com');
-    member.ignore = new RegExp('Concentration', 'i');
-    member.everyMinutes = 15;
-    member.cutTimeRange = [{from: new Date('2020-01-18T12:00'), to: new Date('2020-01-18T13:00')}];
-    member.startDate = new Date('2020-01-20T09:00:00');
-    member.endDate = new Date('2020-01-24T18:00:00');
-    const schedules  = member.fetchSchedules();
+import CaAT from '@silverbirder/caat'
+
+function getMembers(){
+    const id: string = 'testUser@gmail.com';
+    const config: CaAT.IMemberConfig = {
+        startDate: new Date(),
+        endDate: new Date(),
+        everyMinutes: 15,
+        cutTimeRange: [],
+        ignore: new RegExp(''),
+    }
+    const member: CaAT.IMember = new CaAT.Member(id, config);
+    const schedules: Array<CaAT.ISchedule> = member.fetchSchedules();
+    Logger.log(schedules);
 }
