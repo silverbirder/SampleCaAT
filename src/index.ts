6963b0d3c21e6d3a7a6b9315afe4251adbc8e379
shibamoto <silverbirder@gmail.com>
2020-01-24 23:43:58 +0900

    Update the main process

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import * as CaAT from '@silverbirder/caat'
-import {copyDate, getLatestMonday, skipDay} from "./utils";
+import {copyDate, getLatestMonday, getPreviousMonday, getLatestFriday} from "./utils";
 import Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
 import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
 
 function main() {
     // prepare property
-    const userId: string = PropertiesService.getScriptProperties().getProperty('USER_ID');
     const spreadSheetId: string = PropertiesService.getScriptProperties().getProperty('SPREAD_SHEET_ID');
-    const templateSheetName: string = PropertiesService.getScriptProperties().getProperty('TEMPLATE_NAME');
-    if (userId === '') {
-        throw Error('Not set the USER_ID');
-    }
+    const templateSheetName: string = PropertiesService.getScriptProperties().getProperty('TEMPLATE_SHEET_NAME');
     if (spreadSheetId === '') {
         throw Error('Not set the SPREAD_SHEET_ID');
     }
@@ -19,65 +15,40 @@ function main() {
     }
 
     // Prepare date range
-    const monday: Date = new Date(getLatestMonday().setHours(0, 0, 0));
-    const friday: Date = new Date(skipDay(monday, 4).setHours(23, 59, 59));
+    const previousMonday: Date = new Date(getPreviousMonday().setHours(0, 0, 0));
+    const nextMonday: Date = new Date(getLatestMonday().setHours(0, 0, 0));
+    const nextFriday: Date = new Date(getLatestFriday().setHours(23, 59, 59));
 
     // Prepare spread sheet
     const spreadSheet: Spreadsheet = SpreadsheetApp.openById(spreadSheetId);
     const templateSheet: Sheet = spreadSheet.getSheetByName(templateSheetName);
-    const copyToSheetName: string = `${monday.getFullYear()}.${monday.getMonth() + 1}.${monday.getDate()}`;
-    if (spreadSheet.getSheetByName(copyToSheetName)) {
-        throw Error(`${copyToSheetName} Sheet already exists`)
+    const backupSheetName: string = Utilities.formatDate(previousMonday, 'Asia/Tokyo', 'YYYYMMDD');
+    if (spreadSheet.getSheetByName(backupSheetName)) {
+        throw Error(`${backupSheetName} Sheet already exists`)
     }
-    const targetSheet: Sheet = templateSheet.copyTo(spreadSheet);
-    targetSheet.setName(copyToSheetName);
+    spreadSheet.setActiveSheet(templateSheet);
+    spreadSheet.duplicateActiveSheet().setName(backupSheetName);
 
     // Prepare member object
     const cutTimeRange: Array<CaAT.IRange> = [];
-    for (const d = copyDate(monday); d <= friday; d.setDate(d.getDate() + 1)) {
+    for (const d = copyDate(nextMonday); d <= nextFriday; d.setDate(d.getDate() + 1)) {
         cutTimeRange.push({
             from: new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 0, 0),
             to: new Date(d.getFullYear(), d.getMonth(), d.getDate(), 13, 0, 0),
         })
     }
     const config: CaAT.IMemberConfig = {
-        startDate: monday,
-        endDate: friday,
+        startDate: nextMonday,
+        endDate: nextFriday,
         everyMinutes: 15,
         cutTimeRange: cutTimeRange,
         ignore: new RegExp(null),
     };
-    const member: CaAT.IMember = new CaAT.Member(userId, config);
+    const member: CaAT.IMember = new CaAT.Member('', config);
 
     // Fetch the schedules!
     const schedules: Array<CaAT.ISchedule> = member.fetchSchedules();
     const totalAssignMinutes: number = schedules.reduce((totalAssignTime: number, schedule: CaAT.ISchedule): number => {
         return totalAssignTime + schedule.assignMinute;
     }, 0);
-
-    // Set schedules to spread sheet
-    const cell: Array<Array<string>> = [
-        ['ACCOUNT', 'TOTAL ASSIGN HOURS'],
-        [userId, `${totalAssignMinutes/60}`],
-    ];
-    targetSheet.getRange(`A1:B2`).setValues(cell);
-
-    // TODO: Make the group process ...
-    const members: Array<CaAT.IGroupMember> = [{
-        name: userId,
-        match: new RegExp(null),
-    }];
-    const holidayWords: CaAT.IHolidayWords = {
-        morning: new RegExp(null),
-        afternoon: new RegExp(null),
-        all: new RegExp(null),
-    };
-    const groupConfig: CaAT.IGroupConfig = {
-        startDate: monday,
-        endDate: friday,
-        members: members,
-        holidayWords: holidayWords,
-    };
-    const group: CaAT.IGroup = new CaAT.Group(userId, groupConfig);
-    const holidays: Array<CaAT.IHoliday> = group.fetchHolidays();
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,21 @@
-export function getLatestMonday() :Date{
-    const now: Date = new Date();
-    return new Date(now.setDate(now.getDate() - (now.getDay() + 6) % 7 + 7));
+export function getLatestMonday(): Date {
+    const previousMonday: Date = getPreviousMonday();
+    return skipDay(previousMonday, 7);
 }
 
-export function skipDay(d: Date, skip: number): Date{
+export function getPreviousMonday(): Date {
+    const now: Date = new Date();
+    return new Date(now.setDate(now.getDate() - (now.getDay() + 6) % 7));
+}
+
+export function getLatestFriday(): Date {
+    const latestMonday: Date = getLatestMonday();
+    return skipDay(latestMonday, 4);
+}
+
+export function skipDay(d: Date, skip: number): Date {
     const copy: Date = copyDate(d);
-    copy.setDate(copy.getDate()+skip);
+    copy.setDate(copy.getDate() + skip);
     return copy;
 }
 
